547463616ed3d6924ef0e0f20c27d62c97007653
Rodolfo Mora <rodolfomora97@gmail.com>
2024-10-19 12:33:35 -0600

    levantar servidor

---
+++ index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config'   //leer variables entorno
+import express from 'express';
+  
+const app = express();  //server
+
+app.get('/', (req, res) => {
+    res.send('hola mundo')
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('server on'));  //levantar server
